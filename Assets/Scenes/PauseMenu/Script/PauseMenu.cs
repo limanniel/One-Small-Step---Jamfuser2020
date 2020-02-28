e0d5fb991452c5e62432280864f2655647e0b192
Corbin <mottramc@sky.com>
2020-02-28 21:13:59 +0000

    Pause and Start Screen

---
+++ Assets/Scenes/PauseMenu/Script/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject pauseMenu;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Hey");
+    }
+}
---
+++ Assets/StartMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    public static bool startBool = true;
+    public GameObject startGame;
+
+    private void Update()
+    {
+        if(startBool == true)
+        {
+            startGame.SetActive(true);
+        }
+
+        if (Input.anyKeyDown)
+        {
+            StartGame();
+        }
+    }
+
+    public void StartGame()
+    {
+        startGame.SetActive(false);
+        Time.timeScale = 1f;
+        startBool = false;
+    }
+
+}
