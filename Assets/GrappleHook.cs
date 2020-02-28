9fbe031ee50a393b9c8602021b743e38d4db450d
Natalie <nataliekharvey@hotmail.co.uk>
2020-02-28 17:00:01 +0000

    Grapplehook code added

---
+++ Assets/GrappleHook.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappleHook : MonoBehaviour
+{
+    
+    DistanceJoint2D joint;
+    Vector3 targetPos;
+    RaycastHit2D hit;
+    public float distance = 30.0f;
+    public LayerMask mask;
+
+    void Start()
+    {
+        joint = GetComponent<DistanceJoint2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPos.z = 0;
+
+            // Get the player position as origin, the mouse position for direction, and a distance.
+            hit = Physics2D.Raycast(transform.position, targetPos - transform.position, distance, mask);
+            
+            //if hit and the object is a rigid body.
+            if (hit.collider != null && hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                joint.enabled = true;
+                joint.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>();
+                joint.connectedAnchor = hit.point - new Vector2(hit.collider.transform.position.x, hit.collider.transform.position.y);
+                joint.distance = Vector2.Distance(transform.position, hit.point);
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            joint.enabled = false;
+        }
+    }
+}
