7b5708804657c78b3e7ac3ccef28f09df043e763
Tanvir Khan <tanvirk7011@gmail.com>
2020-02-28 20:42:06 +0000

    another short edit

--- Assets/Scripts/PlayerCollisionHandler.cs
+++ Assets/Scripts/PlayerCollisionHandler.cs
@@ -7,5 +7,6 @@ public class PlayerCollisionHandler : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         this.GetComponentInParent<ScriptPlayerController>().HandleCollision(this);
+        
     }
 }
