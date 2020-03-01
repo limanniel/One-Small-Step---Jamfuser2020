70993b5e7c0fb0d22168382d41dd83cd80dc9e42
Sebastian <sebastianploch@protonmail.com>
2020-03-01 05:43:35 +0000

    New movement by force added & jumping

---
+++ Assets/Scripts/Physics2.0/Grounded.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find parent that the script is attached to
+        player = gameObject.transform.parent.gameObject;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            player.GetComponent<PlayerController>().isGrounded = true;
+        }
+    }
+
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            player.GetComponent<PlayerController>().isGrounded = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Physics2.0/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Public Vars
+    public float moveSpeed = 1.0f;
+    public float jumpForce = 1.0f;
+    // Check if character is grounded
+    [HideInInspector]
+    public bool isGrounded;
+
+    // Private vars
+    private Rigidbody2D rigidBody;
+    private bool facingRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rigidBody.AddForce(new Vector2(0.0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Check if rigidbody is assigned/found
+        if (rigidBody)
+        {
+            HorizontalMovement();
+        }
+    }
+
+    void HorizontalMovement()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        Vector2 force = new Vector2(0.0f, 0.0f);
+        force.x = xInput * moveSpeed;
+
+        // Check to not go faster than max speed (moveSpeed)
+        if (rigidBody.velocity.x > moveSpeed)
+        {
+            rigidBody.velocity = new Vector2(moveSpeed, rigidBody.velocity.y);
+            return;
+        }
+
+        rigidBody.AddForce(force);
+    }
+
+    // Flip Facing Direction
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
