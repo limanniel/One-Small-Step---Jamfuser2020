0f45e936313bf964a44c38837f3c2d54976e0982
Sebastian <sebastianploch@protonmail.com>
2020-03-02 05:40:19 +0000

    Added WinScript and WinGoal

---
+++ Assets/Resources/WinLevel/WinScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScript : MonoBehaviour
+{
+    public string LevelToLoadName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene(LevelToLoadName);
+        }
+    }
+}
